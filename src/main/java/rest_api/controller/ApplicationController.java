65ab3cb14fa477258295460a204056830a92e9ad
MFaisalHyder <faisi94@gmail.com>
2016-05-25 00:26:14 +0500

    Application Pages Controller

--- src/main/java/rest_api/controller/ApplicationController.java
+++ src/main/java/rest_api/controller/ApplicationController.java
@@ -1,13 +1,16 @@
 package rest_api.controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
 @RequestMapping("/")
-public class ApplicationController {
+@Configuration
+public class ApplicationController extends WebMvcConfigurerAdapter {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getIndexPage() {
@@ -23,4 +26,15 @@ public class ApplicationController {
 	public ModelAndView getFilePage() {
 		return new ModelAndView("FileManagement");
 	}
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public ModelAndView getUserLoginPage() {
+		return new ModelAndView("Login");
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.GET)
+	public ModelAndView getUserRegisterPage() {
+		return new ModelAndView("Registration");
+	}
+
 }
