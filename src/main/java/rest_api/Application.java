e2c1219791e3c9ec0a7a2535b6f6c3ad5d6bf8fe
MFaisalHyder <faisi94@gmail.com>
2016-07-18 20:29:13 +0500

    Completed

--- src/main/java/rest_api/Application.java
+++ src/main/java/rest_api/Application.java
@@ -1,37 +1,12 @@
 package rest_api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.support.MultipartFilter;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}	
-	
-	@Bean
-    public CommonsMultipartResolver commonsMultipartResolver() {
-        final CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-        commonsMultipartResolver.setMaxUploadSize(-1);
-        return commonsMultipartResolver;
-    }
-
-    @Bean
-    public FilterRegistrationBean multipartFilterRegistrationBean() {
-        final MultipartFilter multipartFilter = new MultipartFilter();
-        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(multipartFilter);
-        filterRegistrationBean.addInitParameter("multipartResolverBeanName", "commonsMultipartResolver");
-        return filterRegistrationBean;
-    }
+	}
 }
