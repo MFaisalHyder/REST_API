64ad066ba37bc5203425b619973394255a0a1016
MFaisalHyder <faisi94@gmail.com>
2016-07-18 20:40:34 +0500

    Completed

--- src/src/main/webapp/WEB-INF/views/CustomerManagement.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Cloud Data Service</title>
-</head>
-<body>
-    <h1>Customer Management Panel</h1>
-</body>
-</html>
--- src/src/main/webapp/static/js/app.js
+++
@@ -1,6 +0,0 @@
-/**
- * file for module.
- * Module in Angular JS is something that holds similar files 
- */
-'use strict';
-var App = angular.module('myApp',[]);
