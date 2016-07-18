f777161554fd4caf61624d39e57ac4b95ef9d9c8
MFaisalHyder <faisi94@gmail.com>
2016-07-18 20:24:49 +0500

    Completed

--- src/main/java/rest_api/model/User.java
+++ src/main/java/rest_api/model/User.java
@@ -24,6 +24,14 @@ public class User {
 		this.age = age;
 	}
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
